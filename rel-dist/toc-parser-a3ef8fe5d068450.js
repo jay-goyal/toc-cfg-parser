7d4924b37c96e4b6cbfcd024979a6d04c569a971
Jay Goyal <goyal.jay2003@gmail.com>
2023-11-08 02:04:32 +0530

    Build follow set code

--- rel-dist/toc-parser-a3ef8fe5d068450.js
+++ rel-dist/toc-parser-a3ef8fe5d068450.js
@@ -627,12 +627,12 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper446 = function(arg0, arg1, arg2) {
-        const ret = makeClosure(arg0, arg1, 115, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper450 = function(arg0, arg1, arg2) {
+        const ret = makeClosure(arg0, arg1, 116, __wbg_adapter_16);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper588 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 187, __wbg_adapter_19);
+    imports.wbg.__wbindgen_closure_wrapper592 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 188, __wbg_adapter_19);
         return addHeapObject(ret);
     };
 
@@ -674,7 +674,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('toc-parser-6661d9182f6ad563_bg.wasm', import.meta.url);
+        input = new URL('toc-parser-a3ef8fe5d068450_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
